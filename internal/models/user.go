a67848f212397ab9f005f2998ecc5ce9b9f21991
Altryd <michael_bor@mail.ru>
2024-12-18 21:10:39 +0400

    Нанесен ластхит (по перхоти) по возможностям JavaScript брыкаться с конвертами uint64 и string

--- internal/models/user.go
+++ internal/models/user.go
@@ -2,7 +2,7 @@ package models
 
 type User struct {
 	OsuId          uint64           `gorm:"primary_key;autoIncrement:false" json:"osu_id"`
-	DiscordId      uint64           `json:"discord_id"`
+	DiscordId      uint64           `json:"discord_id,string"`
 	Rating         uint32           `json:"rating"`
 	Username       string           `json:"username"`
 	Active         bool             `gorm:"default:false" json:"active"`
